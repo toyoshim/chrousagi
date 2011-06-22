9ed6d97ebb2cf4cc22f9fffa7deb91598007076b
toyoshim <toyoshim@77d0f05a-0ac3-9dee-5433-d8e82e09e206>
2011-06-22 15:07:37 +0000

    Add diary check

---
+++ trunk/diary.js
@@ -0,0 +1,46 @@
+function Diary () {
+    this._url = "http://www.tamurayukari.com/diary/index.html";
+    this._id = "N/A";
+}
+Diary.prototype.Fetch = function (callback, args) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", this._url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState != 4)
+            return;
+        var dom = document.implementation.createHTMLDocument("");
+        var range = dom.createRange();
+        range.selectNodeContents(dom.documentElement);
+        range.deleteContents();
+        var node = range.createContextualFragment(xhr.responseText);
+        dom.documentElement.appendChild(node);
+        if (callback)
+            callback(dom, args);
+    };
+    xhr.send();
+};
+Diary.prototype.Notify = function (entry) {
+    var title = entry.getElementsByClassName("title")[0].textContent;
+    var date = entry.getElementsByClassName("date")[0].textContent;
+    var text = entry.getElementsByClassName("text-box")[0].textContent;
+    var notification = webkitNotifications.createNotification(
+            "icon.png",
+            title + " (" + date + ") from Diary",
+            text);
+    notification.show();
+};
+Diary.prototype.Check = function (callback, args) {
+    this.Fetch(function (dom, self) {
+        var diary = dom.getElementById("diary-box");
+        var entry = diary.getElementsByClassName("entry-box")[0];
+        var title = entry.getElementsByClassName("title")[0].textContent;
+        var date = entry.getElementsByClassName("date")[0].textContent;
+        var id = title + date;
+        if (id && id != self._id) {
+            self._id = id;
+            self.Notify(entry);
+        }
+        if (callback)
+            callback(args);
+    }, this);
+};
